e3e54c5b4b0532e3bdc47ccfc1ebb2bf47b4d190
nandhaa-anu <chrohith8099991699@gmail.com>
2026-03-13 11:41:02 +0530

    Update firebaseConfig.js
    Added real Firebase configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+   apiKey: "AIzaSyD3OaVl61SU2GY2NrcBxFmsWbt-91r5s7k",
+  authDomain: "vishnu-event-hub-fc4d0.firebaseapp.com",
+  projectId: "vishnu-event-hub-fc4d0",
+  storageBucket: "vishnu-event-hub-fc4d0.firebasestorage.app",
+  messagingSenderId: "1071647901731",
+  appId: "1:1071647901731:web:c014b8bed16813a678c996"
 };
 
 export default firebaseConfig;
